6c4c6b4213576289808c4442d5789b612f5e38dc
dvdsp <131421834+dvdsp@users.noreply.github.com>
2023-10-21 16:29:20 +0700

    Add files via upload

---
+++ src/Controller/AnalyzeUserInputManager.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class AnalyzeUserInputManager {
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void getNumber(String inputString) {
+        HashMap<String, ArrayList<Integer>> hmNumber = new HashMap<>();
+        String number = inputString.replaceAll("\\D+", ",");
+        if (number.charAt(0) == ',') {
+            number = number.substring(1);
+        }
+        if (number.charAt(number.length() - 1) == ',') {
+            number = number.substring(0, number.length() - 1);
+        }
+        String[] listNumber = number.split(",");
+        int lenNumber = listNumber.length;
+
+        ArrayList<Integer> liPerfectSquare = new ArrayList<>();
+        ArrayList<Integer> liOdd = new ArrayList<>();
+        ArrayList<Integer> liEven = new ArrayList<>();
+        ArrayList<Integer> liAll = new ArrayList<>();
+        for (int i = 0; i < lenNumber; i++) {
+            int numberCheck = Integer.parseInt(listNumber[i]);
+            if (numberCheck % 2 == 1) {
+                liOdd.add(numberCheck);
+            }
+            if (numberCheck % 2 == 0) {
+                liEven.add(numberCheck);
+            }
+            if (checkSquareNumber(numberCheck)) {
+                liPerfectSquare.add(numberCheck);
+            }
+            liAll.add(numberCheck);
+        }
+        hmNumber.put("Perfect Square Numbers: ", liPerfectSquare);
+        hmNumber.put("Odd Numbers: ", liOdd);
+        hmNumber.put("Even Numbers: ", liEven);
+        hmNumber.put("All Numbers: ", liAll);
+
+        for (Map.Entry m : hmNumber.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+    }
+    private static boolean checkSquareNumber(int n) {
+        if (Math.sqrt(n) * Math.sqrt(n) == n) {
+            return true;
+        }
+        return false;
+    }
+    public static void getCharacter(String inputString) {
+        HashMap<String, String> hmString = new HashMap<>();
+        String uppercase = inputString.replaceAll("\\W|[0-9]|[a-z]", "");
+        String lowercase = inputString.replaceAll("\\W|[0-9]|[A-Z]", "");
+        String special = inputString.replaceAll("\\w", "");
+        String allCharacter = inputString.replaceAll("\\W", "");
+        hmString.put("Uppercase: ", uppercase);
+        hmString.put("Lowercase: ", lowercase);
+        hmString.put("Special: ", special);
+        hmString.put("All Character: ", allCharacter);
+        for (Map.Entry m : hmString.entrySet()) {
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("\tAnalysis String program");
+        getNumber("617kshid456jjsossFJJSNOWNSMODHB!@#$%^&*(){}[]");
+        getCharacter("617kshid456jjsossFJJSNOWNSMODHB!@#$%^&*(){}[]");
+            
+    }
+    
+}
